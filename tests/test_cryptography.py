46c6d9759eccbe4dcf1b3c7709b434a85a82a825
Anton <antsht@gmail.com>
2024-03-06 16:13:41 +0300

    Fix

--- tests/test_cryptography.py
+++ tests/test_cryptography.py
@@ -16,8 +16,8 @@ def test_cryptography():
     file = "tests/cryptography/chifre_me.txt"
     result = run([B_PATH_FILE], input=f"{file}", encoding='utf-8', stdout=PIPE)
     assert result.stdout == ""
-    file_data = run([f"cat {file}"], stdout=PIPE)
-    assert file_data.stdout == "x ficmhpt m grr ifn m fvplyki m teq ndygy xky hoc"
+    file_data = run(['cat', f"{file}"], encoding='utf-8', stdout=PIPE)
+    assert file_data.stdout == "x ficmhpt m GRR iFn m fvpLyKi m teQ ndYgy Xky HOC"
 
 if __name__ == "__main__":
     pytest.main()
