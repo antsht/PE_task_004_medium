5e63c683aa4497fe0094092af8480cfdd9fa39cd
Anton <antsht@gmail.com>
2024-03-06 13:50:51 +0300

    Fix

--- src/cryptography.c
+++ src/cryptography.c
@@ -4,7 +4,6 @@ int main(void) {
     ErrorCode error;
     char file_path[MAX_NAME_LEN];
     try(error = get_file_path(file_path));
-    printf("file_path: %s\n", file_path);
     try(error = encrypt_file(file_path));
 
     return 0;
